bfceaa81abc37acd800fbdaf3f368b92ede78e03
krisnafirdaus <krisnafirdaus@gmail.com>
2022-05-17 10:11:21 +0800

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import Grader from "./component/Grader";
+
+export const ThemeContext = createContext();
 
 function App() {
+  const [theme, setTheme] = useState("summer");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider
+      value={{
+        theme: theme,
+        setTheme: setTheme,
+      }}
+    >
+      {/* <ToolBar />
+      <WeatherReaction /> */}
+      <Grader />
+    </ThemeContext.Provider>
   );
 }
 
